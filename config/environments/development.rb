05a06d533a934ce333039ff7dcfd8877a97655eb
hiroya.shimamoto <shshimamo@gmail.com>
2019-12-23 00:37:44 +0900

    Make it possible to access from outside the container

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Dockerコンテナ外からの接続を許可する
+  config.web_console.whitelisted_pids = %w(172.16.0.0/12)
 end
