11e0fa82f4b7554283e0f88fb5e07a0e864e449b
hiroya.shimamoto <shshimamo@gmail.com>
2019-12-23 00:35:26 +0900

    Make it possible to remove pid file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,11 @@ bundle check || true
 # Yarnの依存関係をチェックしておく
 yarn check --integrity --silent || true
 
+# Railsサーバーを実行する場合PIDファイルがあれば削除しておく
+if [ "${1:-}" = rails -a "${2:-}" = server ]; then
+  if [ -f tmp/pids/server.pid ]; then
+    rm -v tmp/pids/server.pid
+  fi
+fi
+
 exec "$@"
